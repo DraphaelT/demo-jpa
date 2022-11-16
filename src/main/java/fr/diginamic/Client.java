cc44768ceb06d47e25f5d7e85be0939bf44befa6
DraphaelT <115068828+DraphaelT@users.noreply.github.com>
2022-11-16 09:57:15 +0100

    MAJ

--- src/main/java/fr/diginamic/Client.java
+++ src/main/java/fr/diginamic/Client.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
@@ -24,8 +25,9 @@ public class Client {
 	@Column(name = "PRENOM", length = 50, nullable = false)
 	private String prenom;
 	
-	@OneToMany(mappedBy="client")
-	private Set<Emprunt> emprunts;
+	@ManyToOne
+	@JoinColumn(name = "ID_CLIENT")
+	private Client client;
 	
 	// constructeur sans argument
 	public Client() {
